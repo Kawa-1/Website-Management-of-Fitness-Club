5fd682a7e69df94aa2b5acdddfb8902e2c13b429
Kawa-1 <kamil-kawka@outlook.com>
2022-01-19 01:57:35 +0100

    Added models.py; ORM

--- myapplication/__init__.py
+++ myapplication/__init__.py
@@ -1,23 +1,29 @@
 from myapplication.logger import get_logger
 from myapplication.config import YamlConf
 from myapplication.security import CryptoKey
-from myapplication.db import db_before_request, db_init_app, get_cursor, init_tables
+# from myapplication.db import db_before_request, db_init_app, get_cursor, init_tables
 from flask import Flask, jsonify, g, request, json
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from flask_migrate import Migrate
 from werkzeug.exceptions import HTTPException
 from datetime import datetime
 
 
 log = get_logger(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     #log.info("Starting server...")
     app = Flask(__name__)
     api = Api(app)
+
     app.config.from_mapping(SECRET_KEY='dev')
 
     crypt_ob = CryptoKey()
-    app.config['CryptoKey'] = crypt_ob
+    app.config['CRYPTO_KEY'] = crypt_ob
 
     conf_db = YamlConf.get_yaml_postgres()
     app.config['DB_DRIVER'] = conf_db['driver']
@@ -28,9 +34,20 @@ def create_app():
     app.config['DB_PASSWORD'] = conf_db['password']
     app.config['DB_PORT'] = conf_db['port']
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{app.config['DB_PASSWORD']}@localhost:5432/fitness"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #db.engine.execute("grant usage on schema public to public;")
+    #db.engine.execute("grant create on schema public to public;")
+
+    from myapplication.models import Users, Instructors, Facilities, Subscriptions, PriceList, Classes, Participation
+    db.init_app(app)
+    migrate.init_app(app, db)
+    db.create_all(app=app)
+
     #db_init_app(app) Tu jest błąd musimy go rozwiązać elo
 
-    app.before_request(db_before_request)
+    #app.before_request(db_before_request)
 
     @app.errorhandler(HTTPException)
     def not_found(e):
@@ -45,14 +62,18 @@ def create_app():
         response.content_type = "application/json"
         response.code = e.code
         response.content_language = "en"
-
         return response
 
     @app.route('/', methods=['GET'])
+    def hello():
+        r = db.engine.execute('SELECT * FROM users;')
+        return "<h1>HELLOOOOOO!!!</h1>"
+
+    @app.route('/hello', methods=['GET'])
     def hello_world():
-        init_tables()
-        x = get_cursor().execute("""SELECT * FROM users""")
-        print(x)
+        #init_tables()
+        #x = get_cursor().execute("""SELECT * FROM users""")
+        #print(x)
         return f'Hello World\n {x}'
 
     return app
---
+++ myapplication/models.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+from myapplication import db
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    house_number = db.Column(db.Integer, nullable=False)
+    postcode = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(600), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    user_participations = db.relationship('Participation', backref='user_participations', lazy=True)
+    user_subscriptions = db.relationship('Subscription', backref='user_subscriptions', lazy=True)
+
+
+class Instructors(db.Model):
+    __tablename__ = 'instructors'
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    house_number = db.Column(db.Integer, nullable=False)
+    postcode = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(600), nullable=False)
+
+    instructor_classes = db.relationship('Classes', backref='instructor_classes', lazy=True)
+
+
+class Facilities(db.Model):
+    __tablename__ = 'facilities'
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(100), nullable=False)
+    street = db.Column(db.String(100), nullable=False)
+    house_number = db.Column(db.Integer, nullable=False)
+    postcode = db.Column(db.String(100), nullable=False)
+    contact_number = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+    facility_classes = db.relationship('Classes', backref='facility_activity', lazy=True)
+    facility_subscription = db.relationship('Subscriptions', backref='facility_subscription', lazy=True)
+
+
+class PriceList(db.Model):
+    __tablename__ = 'price_list'
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+
+class Subscriptions(db.Model):
+    __tablename__ = "subscriptions"
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(100), nullable=False)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('fit.users.id'))
+    facility_id = db.Column(db.Integer, db.ForeignKey('fit.facilities.id'))
+    price_id = db.Column(db.Integer, db.ForeignKey('fit.price_list.id'))
+
+
+class Classes(db.Model):
+    __tablename__ = "classes"
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    type_of_classes = db.Column(db.String(100), nullable=False)
+
+    instructor_id = db.Column(db.Integer, db.ForeignKey('fit.instructors.id'))
+    facility_id = db.Column(db.Integer, db.ForeignKey('fit.facilities.id'))
+    price_id = db.Column(db.Integer, db.ForeignKey('fit.price_list.id'))
+
+
+class Participation(db.Model):
+    __tablename__ = 'participation'
+    __table_args__ = {'extend_existing': True, 'schema': 'fit'}
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('fit.users.id'))
+    class_id = db.Column(db.Integer, db.ForeignKey('fit.classes.id'))
+
+
+#db.create_all()
