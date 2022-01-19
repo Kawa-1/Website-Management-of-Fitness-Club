588823866458bd41771ba593ebb47e966b2ce08b
jfgtf <ballscrusher.jk@gmail.com>
2022-01-20 00:06:46 +0100

    angular initialization + login and registration

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { ToastrService } from 'ngx-toastr';
+import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+// import { User } from '../models/User';
+// import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor( 
+    // private auth: AuthService, 
+    private cookieService: CookieService, 
+    private toastr: ToastrService
+  ) {}
+
+  // user: User = new User();
+
+  onLogin(email:string, password:string): void {
+    // this.user.email = email;
+    // this.user.password = password;
+
+    // this.auth.login(this.user)
+    // .then((user) => {
+    //   this.cookieService.set('token', user.token);
+    //   this.toastr.success('Logged in succesfully');
+    // });
+  }
+
+  clicked(email:string, password:string){
+    this.onLogin(email, password);
+  }
+}
---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,53 @@
+import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
+// import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+
+export class RegisterComponent implements OnInit{ 
+  constructor( 
+    private toastr: ToastrService, 
+    // private auth: AuthService 
+  ) {}
+ 
+  isDisabled = true;
+  passwordPattern= "";
+
+  ngOnInit(){}
+
+  clicked(email:string, firstName:string, lastName:string, username:string, password:string, repeatPassword: string){
+    var formData: any = new FormData();
+
+    formData.append("email", email);
+    formData.append("firstName", firstName);
+    formData.append("lastName", lastName);
+    formData.append("username", username);
+    formData.append("password", password);
+    formData.append("repeatPassword", repeatPassword);
+
+    // this.auth.register(formData)
+    // .subscribe(
+    //   data => {
+    //       this.toastr.success('Registered successfully');
+    //   });
+
+    formData.delete("email");
+    formData.delete("firstName");
+    formData.delete("lastName");
+    formData.delete("username");
+    formData.delete("password");
+    formData.delete("repeatPassword");
+  }
+
+  getPassword(s:string){
+    this.passwordPattern = s;
+  }
+
+  getMsgFromBaby() {
+    this.isDisabled = false;
+  }
+}
