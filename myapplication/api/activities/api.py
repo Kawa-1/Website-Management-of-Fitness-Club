ee509480adbe9b2cb0abf367d7a74ccbe89aac11
Kawa-1 <kamil-kawka@outlook.com>
2022-02-18 13:07:16 +0100

    Added new parameter to /api/activity_api requested by frontend

--- myapplication/api/activities/api.py
+++ myapplication/api/activities/api.py
@@ -15,10 +15,14 @@ log = get_logger(__name__)
 # class involved with activities (classes ~ activity involved with fitness) table
 class ActivityApi(Resource):
     #@token_required
-    #g.user = None
+
     @limit_offset
-    def get(self, date=None):
-        """Date parameter should be passed like: Year_Month_Day e.g. 2021_02_09 (YYYY_MM_DD) then we will receive all acitivites during this day
+    def get(self, date=None, since_today=None):
+        """
+        Date parameter should be passed like: Year_Month_Day e.g. 2021_02_09 (YYYY_MM_DD) then we will receive all acitivites during this day
+        OR
+        We are able to pass parameter 'since_today' with value 1 (only 1 is processable) then we will get all activities since today
+        Both parameters will result in bad request...
         if we are not passing any date parameter:
             then we will receive all activities from db
         if we are passing date like: 2020_12:
@@ -26,9 +30,59 @@ class ActivityApi(Resource):
         if we are passing date like: 2020:
             then we will receive all activities from the year of 2020
 
-        NOTE: Return is sorted; from the oldest to the newest
+        NOTE: Return is sorted; from the oldest to the newest in case of date parameter,
+                                from the newest to the oldest in case of since_today parameter
         """
         date: str
+        since_today: int
+
+        if since_today is not None:
+            if date is not None:
+                err_resp = {"message": {
+                    "description": "It is not allowed to pass simultaneously parameter date and since_today",
+                    "method": "GET", "name": "Failed obtaining activities",
+                    "status": 400, "timestamp": timestamp}}
+                return err_resp, 400
+
+            if since_today == 1:
+                today = "{} {}".format(datetime.utcnow().strftime("%Y-%m-%d"), "00-00")
+                cmd = """SELECT a.date, t.name_of_service, f.city, f.street, f.house_number, p.price, u.first_name, u.last_name, u.email, 
+                                    (SELECT COUNT(p.id) FROM fit.participation p WHERE p.activity_id=a.id), a.id
+                                    FROM fit.activities a
+                                    INNER JOIN fit.facilities f ON a.facility_id=f.id
+                                    INNER JOIN fit.price_list p ON a.price_id=p.id
+                                    INNER JOIN fit.users u ON u.is_instructor=1 AND u.id=a.instructor_id
+                                    INNER JOIN fit.types_of_services t ON t.id=a.type_of_service_id AND t.is_subscription=0
+                                    WHERE \'%s\'<=a.date
+                                    ORDER BY a.date 
+                                    LIMIT %d""" % (today ,g.limit)
+
+                activity = db.session.execute(cmd).cursor.fetchall()
+                if len(activity) == 0:
+                    resp = {"message": {
+                        "description": "There are no activities at all",
+                        "status": 200, "name": "lack of activities", "method": "GET", "timestamp": timestamp}}
+                    # 204 should be
+                    return resp, 200
+
+                dictionary = {
+                    "message": {"description": "Activities returned", "name": "Activities returned", "status": 200,
+                                "method": "GET", "timestamp": timestamp}, "activities": []}
+                for obj in activity:
+                    dictionary['activities'].append(
+                        {"date": obj[0], 'type_of_service': obj[1], 'city': obj[2], 'street': obj[3],
+                         'house_number': obj[4], 'price': obj[5], 'first_name': obj[6],
+                         'last_name': obj[7], 'email': obj[8], "total_number_of_users": obj[9], "id": obj[10]})
+
+                return dictionary, 200
+
+            else:
+                err_resp = {"message": {"description": "improper type/format of parameter since_today only int(1) is processable",
+                                        "method": "GET", "name": "Failed obtaining activities which are taking place since today",
+                                        "status": 422, "timestamp": timestamp}}
+                return err_resp, 422
+
+
 
         if date is None:
             cmd = """SELECT a.date, t.name_of_service, f.city, f.street, f.house_number, p.price, u.first_name, u.last_name, u.email, 
@@ -60,11 +114,11 @@ class ActivityApi(Resource):
             #dictionary = json.dumps(dictionary, indent=4, sort_keys=True)
             return dictionary, 200
 
-        err_resp = {"errors": [{"description": "Provided bad format of date",
-                                "method": "GET", "name": "Failed obtaining activities", "status": 422, "timestamp": timestamp}]}
-
         date = date.replace("_", "-")
         if not check_date_format(date) or not check_month_format(date) or not check_year_format(date):
+            err_resp = {"message": {"description": "Provided bad format of date",
+                                    "method": "GET", "name": "Failed obtaining activities", "status": 422,
+                                    "timestamp": timestamp}}
             return err_resp, 422
 
         cmd = """SELECT a.date, t.name_of_service, f.city, f.street, f.house_number, p.price, u.first_name, u.last_name, u.email,
